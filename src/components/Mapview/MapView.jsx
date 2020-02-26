a227e303c0e9c033ca0022234f3fb31f1bb9f67b
mai <58199372+maiqvu@users.noreply.github.com>
2020-02-26 22:22:30 +1100

    edited Map component to display real data

--- src/components/Mapview/MapView.jsx
+++ src/components/Mapview/MapView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 // import { FirebaseContext } from '../Firebase/index';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 // import CurrentLocation from './MapConfig'; // TODO: recenter map to user's location
@@ -13,31 +14,34 @@ export class MapView extends React.Component {
     showingInfoWindow: false,
     selectedMarker: {},   // show the active marker upon click
     selectedPlace: {},   // show InfoWindow for the selected place
-    restaurants: [
-      { lat: -33.8704, lng: 151.2087, name: '1' },
-      { lat: -33.8749, lng: 151.2009, name: '2' },
-      { lat: -33.8611, lng: 151.2126, name: '3' },
-      { lat: -33.8599, lng: 151.2090, name: '4' },
-      { lat: -33.8435, lng: 151.2413, name: '5' }
-    ]
+    restaurants: []
   };
 
   mapStyles = { width: '100vw', height: '100vh' };
   sydney = { lat: -33.8688, lng: 151.2093 };
   defaultMarker = {
     url: 'https://cdn0.iconfinder.com/data/icons/travel-vacation/290/travel-transport-hotel-vacation-holidays-tourist-tourism-travelling-traveling_149-512.png',
-    scaledSize: new this.props.google.maps.Size(40, 40)
+    scaledSize: new this.props.google.maps.Size(20, 20)
   };
   colorMarker = {
       url: 'https://www.hentiesbaytourism.com/wp-content/uploads/2016/06/restaurant_marker.png',
       scaledSize: new this.props.google.maps.Size(60, 60)
   };
 
+  componentDidMount() {
+    axios.get( 'https://droolworthy-sydney.firebaseio.com/restos.json?orderBy=%22$key%22&limitToFirst=2&print=pretty' )
+      .then( res => {
+        console.log(res.data[0].restaurants);
+        this.setState({ restaurants: res.data[0].restaurants });
+      })
+      .catch( err => console.warn(err) );
+  }
+
   // To initially display all markers on map
   displayMarkers = () => {
-    return this.state.restaurants.map( (restaurant, index) => {
-      return <Marker key={index} name={ restaurant.name }
-        position={{ lat: restaurant.lat, lng: restaurant.lng }}
+    return this.state.restaurants.map( (resto, index) => {
+      return <Marker key={index} name={ resto.restaurant.name }
+        position={{ lat: resto.restaurant.location.latitude, lng: resto.restaurant.location.longitude }}
         onClick={ this.handleMarkerClick }
         icon={ this.colorMarker } />
     });
@@ -64,18 +68,10 @@ export class MapView extends React.Component {
   };
 
   render() {
-    // const displayMarkers = this.state.restaurants.map( (restaurant, index) => {
-    //   return <Marker key={index} name={ restaurant.name }
-    //     position={{ lat: restaurant.lat, lng: restaurant.lng }}
-    //     onClick={ this.handleMarkerClick }
-    //     icon={ (!this.state.selectedMarker) ? defaultMarker : selectedMarker } />
-    // });
-
     return (
-      <Map google={ this.props.google } zoom={14} initialCenter={ this.sydney } style={ this.mapStyles }>
+      <Map google={ this.props.google } zoom={12} initialCenter={ this.sydney } style={ this.mapStyles }>
 
         { this.displayMarkers() }
-        {/* { displayMarkers } */}
 
         <InfoWindow visible={ this.state.showingInfoWindow }
                   onClose={ this.handleClose }
